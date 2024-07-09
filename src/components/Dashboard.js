d9bd22b22258a1f5ccfe9c2d77c8ae2788076731
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-07-09 12:32:41 -0400

    create Panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,28 @@
 import React, { Component } from "react";
 
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads"",
+    value: “Lukas Souza”
+  }
+];
+
 import classnames from "classnames";
 
 class Dashboard extends Component {
